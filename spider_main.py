4b9c73f7cee20b454fdd320a229b4dc4e83b3673
Eaphan L <36919957+fangfucdwin@users.noreply.github.com>
2018-07-06 11:08:48 +0800

    Update spider_main.py

--- spider_main.py
+++ spider_main.py
@@ -1,4 +1,9 @@
-import html_downloader, html_parser, html_outputer
+import time
+import html_parser
+import html_outputer
+import html_downloader
+from config import *
+
 
 
 class SpiderMain(object):
@@ -9,43 +14,64 @@ class SpiderMain(object):
         self.outputer = html_outputer.HtmlOutputer()  # 输出器
 
     def craw(self, base_url):
-        count = 1
         try:
-            print('crawing %d ' % count)
+            start_base = time.perf_counter()
+            print('crawing base_url')
+
             html_cont_base = self.downloader.download_base(base_url)
             new_data = self.parser.parse_base(html_cont_base)[0]
             self.outputer.collect_data(new_data)
+
+            end_base = time.perf_counter() - start_base
+            print('crawed base_url %.2fs' % end_base)
+
             has_next_page = self.parser.parse_base(html_cont_base)[1]
             end_cursor = self.parser.parse_base(html_cont_base)[2]
 
             var_value = {"id": "", "first": 12, "after": ""}
             var_value['id'] = self.parser.parse_base(html_cont_base)[3]
-            count = 2
+
+            count = 1
 
             while has_next_page:
 
-                print('crawing %d ' % count)
+                start_next = time.perf_counter()
+                print('\ncrawing next_url_%d ' % count)
+
                 var_value['after'] = end_cursor
                 next_url = self.parser.get_next_url(var_value)
                 html_cont_next = self.downloader.download_next(next_url)
                 new_data = self.parser.parse_next(html_cont_next)[0]
                 self.outputer.collect_data(new_data)
+
+                end_next = time.perf_counter() - start_next
+                print('crawed next_url_%d %.2fs' % (count, end_next))
+
                 has_next_page = self.parser.parse_next(html_cont_next)[1]
                 end_cursor = self.parser.parse_next(html_cont_next)[2]
 
-                if count >= 5:
+                if count >= PAGE_COUNT:
                     break
+
+
                 count += 1
+
 
         except Exception as e:
             print(str(e))
             # 根据报错信息提示错误
-
-        self.outputer.output_html()
-
+        
+        self.outputer.output_html() #以html格式输出
+        self.outputer.save_to_mongo() #保存在mongodb中
+        self.outputer.save_image() #将图片保存到本地
 
 
 if __name__ == '__main__':
-    base_url = 'https://www.instagram.com/nasa/'
+    start_main = time.perf_counter()
+
     obj_spider = SpiderMain()
-    obj_spider.craw(base_url)
+    obj_spider.craw(BASE_URL)
+
+    end_main = time.perf_counter() - start_main
+
+    print('\ncrawed %.2fs' % end_main)
