c3e1cea0e45553151f74541e8b8ee0ffa3c879eb
Eaphan L <36919957+fangfucdwin@users.noreply.github.com>
2018-07-06 11:10:03 +0800

    Update html_outputer.py

--- html_outputer.py
+++ html_outputer.py
@@ -1,3 +1,17 @@
+import os
+
+import pymongo
+
+
+from hashlib import md5
+
+import requests
+
+from config import *
+
+
+
+
 class HtmlOutputer(object):
     def __init__(self):
         self.datas = []
@@ -17,7 +31,7 @@ class HtmlOutputer(object):
 
             fout.write("<li>")
 
-            fout.write('<a href="%s" target="_blank">%s</a>' % (data['URL'],num))
+            fout.write('<a href="%s" target="_blank">%s</a>' % (data['URL'], num))
             fout.write('<img src="%s" width="640" height="640">' % data['URL'])
             fout.write("<br /><b>LikeCount:%s</b><br />" % data['Like_Count'])
             fout.write("<b>CommenCount:%s</b>" % data['Commen_Count'])
@@ -29,3 +43,41 @@ class HtmlOutputer(object):
         fout.write("</body>")
         fout.write("</html>")
         fout.close()
+
+    def save_to_mongo(self):
+        client = pymongo.MongoClient(MONGO_URL)
+        db = client[DATEBASE_DB]
+        count = 1
+        for data in self.datas:
+            if db[DATEBASE_TABLE].insert(data):
+                print('存储第{0:d}条内容到Mongo成功'.format(count), data['URL'])
+            count += 1
+        return True
+
+    def save_image(self):
+        count = 1
+        for data in self.datas:
+            print('正在下载第{0:d}张图片'.format(count), data['URL'])
+
+            if data['URL'] is None:
+                return None
+
+            kv = {
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3423.2 Safari/537.36',
+                'referer': 'https://www.instagram.com/',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+                'cookie': '用户登录的cookie'
+            }
+            response = requests.get(data['URL'], headers=kv)
+            response.encoding = response.apparent_encoding
+
+            if response.status_code != 200:
+                return None
+            file_path = '{0}/{1}.{2}'.format(os.getcwd(), md5(response.content).hexdigest(), 'jpg')
+            if not os.path.exists(file_path):
+                with open(file_path, 'wb') as f:
+                    f.write(response.content)
+                    f.close()
+            count += 1
